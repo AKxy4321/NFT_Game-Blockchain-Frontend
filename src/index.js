8c117dc996156676abb4ea70fa550c21020a4151
Aditya <adityakushal23@gmail.com>
2023-11-22 22:58:10 +0530

    o

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Components/SelectCharacter/SelectCharacter.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -9,3 +10,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+const SelectCharacter = ({ setCharacterNFT }) => {
+  return (
+    <div className="select-character-container">
+      <h2>Mint Your Hero. Choose wisely.</h2>
+    </div>
+  );
+};
+
+export default SelectCharacter;
