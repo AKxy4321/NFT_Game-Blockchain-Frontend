bd4da1a5998fea9314a604fc82ad240770b260c0
Aditya <adityakushal23@gmail.com>
2023-11-23 03:02:12 +0530

    o

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const CONTRACT_ADDRESS = '0x5100528DD36A21BE97b635e7284Cbd07cafc99c3';
+const CONTRACT_ADDRESS = '0x1f152F29F330b8Ce04eE92dd1A0424e418e92A92';
 
 const transformCharacterData = (characterData) => {
   return {
